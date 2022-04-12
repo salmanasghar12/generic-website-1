c87dfe54b486248d88a6f0ad8202ee9b52520121
salmanasghar12 <iamsalmanasghar@gmail.com>
2022-04-12 20:38:43 +0500

    Animation on Scroll has been added

--- script.js
+++ script.js
@@ -12,3 +12,26 @@ mobileBtn.addEventListener('click', () => {
 mobileBtnExit.addEventListener('click', () => {
      nav.classList.remove('menu-btn');
 })
+
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+
+     let reveals = document.querySelectorAll('.reveal');
+
+
+     for(let i=0; i<reveals.length; i++){
+          let windowheight = window.innerHeight;
+          let revealtop = reveals[i].getBoundingClientRect().top;
+
+          let revealpoint = 200;
+
+if (revealtop < windowheight - revealpoint){
+     reveals[i].classList.add('active');
+}
+else {
+     reveals[i].classList.remove('active');
+}
+     }
+
+}
